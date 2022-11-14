90ab5807f761d7cb0dab2e8e93e8ed83fa331f37
Ana Luiza <analuizamdv@gmail.com>
2022-11-14 16:55:43 -0300

    added scroll reveal

--- script.js
+++ script.js
@@ -11,3 +11,9 @@ document.querySelectorAll('.maisOpcoes-item').forEach(n => n.addEventListener('c
   floatMenu.classList.remove('active');
   overlay.classList.remove('active');
 }))
+
+// Scroll reveal library
+ScrollReveal().reveal('.area-1');
+ScrollReveal().reveal('.area-2', { delay: 100, distance: '50px'});
+ScrollReveal().reveal('.area-3', { distance: '50px'});
+ScrollReveal().reveal('.area-4', { distance: '50px'});
